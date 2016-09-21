6e37de3023f24be877738ab2bd65eef7c9cabe12
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2016-09-21 13:59:46 +0200

    Dont't build paralution examples

--- install-paralution.sh
+++ install-paralution.sh
@@ -2,10 +2,13 @@
 set -ev
 wget http://www.paralution.com/downloads/paralution-1.1.0.tar.gz
 tar xzf paralution-1.1.0.tar.gz
+# Bugfix for paralution
 patch -p0 < paralution.patch
+# Switch OpenCL off for mac os build
 patch -p0 < paralution-cmake.patch
 cd paralution-1.1.0
 mkdir build
 cd build
-cmake ..
+# Dont't build examples
+cmake -DBUILD_EXAMPLES=OFF ..
 make -j2
