afd1b789dab1371e52322a7566e9d7d17803b557
Kalle0x12 <Kalle0x12@users.noreply.github.com>
2017-03-03 18:59:17 +0100

    Fix gcc LTO flags

--- install-pybind11.sh
+++ install-pybind11.sh
@@ -2,5 +2,5 @@
 set -ex
 git clone https://github.com/pybind/pybind11.git
 cd pybind11
-cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 -DPYBIND11_WERROR=OFF
+cmake  -DPYBIND11_PYTHON_VERSION=$1 -DPYBIND11_CPP_STANDARD=$2 $3 -DPYBIND11_WERROR=OFF
 make -j2 pytest
